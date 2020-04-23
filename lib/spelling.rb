ffdc800b0ccf5b8747d333392e692e903298683e
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-04-23 09:11:24 +0100

    Refactor to return input

--- lib/spelling.rb
+++ lib/spelling.rb
@@ -1,5 +1,5 @@
 class Spelling
   def check(input)
-    "Cat"
+    input
   end
 end
--- spec/spelling_spec.rb
+++ spec/spelling_spec.rb
@@ -5,5 +5,9 @@ describe Spelling do
     it 'should take an input of "Cat" and return "Cat"' do
       expect(subject.check("Cat")).to eq ("Cat")
     end
+
+    it 'should take an input of "Dog" and return "Dog"' do
+      expect(subject.check("Dog")).to eq ("Dog")
+    end
   end
 end
